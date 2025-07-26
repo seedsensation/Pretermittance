13999e8b6ad7dc6f80837fb6dbc4905a53746ec6
seeds <seedsensation@icloud.com>
2025-07-27 00:13:11 +0100

    Fixed jittery movement

--- scripts/Character.cs
+++ scripts/Character.cs
@@ -3,7 +3,7 @@ using System;
 
 public partial class Character : CharacterBody2D
 {
-	public const float Speed = 15;
+	public const float Speed = 20;
 	public const float JumpVelocity = -400.0f;
 
 	public override void _PhysicsProcess(double delta)
